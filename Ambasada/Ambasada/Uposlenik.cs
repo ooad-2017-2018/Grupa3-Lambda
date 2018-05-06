9b1c4d56cfe002817465f51ca9b17fc54dad06c5
Ali Dlakic <ali@pro.ba>
2018-05-06 17:16:00 +0200

    dodan file zato sto visual studio uporno ignorise fajlove

---
+++ Ambasada/Ambasada/Uposlenik.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Ambasada
+{
+    class Uposlenik
+    {
+        public string Username { get; set; }
+
+        public string Password { get; set; }
+
+        public bool Administrator { get; set; }
+
+        public override string ToString() {
+            return Username;
+        }
+    }
+}
