deccd6f33fc172d11d72d6dddd7e170de6c2bd2d
ddzigal1 <ddzigal1@etf.unsa.ba>
2018-05-06 12:39:09 +0200

    :))

--- Ambasada/Ambasada/BrisanjeAzuriranjeRacuna.xaml.cs
+++ Ambasada/Ambasada/BrisanjeAzuriranjeRacuna.xaml.cs
@@ -26,5 +26,10 @@ namespace Ambasada
         {
             this.InitializeComponent();
         }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
     }
 }
