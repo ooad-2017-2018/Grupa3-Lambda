adc5b6be628e1b9ee7c7f4d35e2e24555e263c8c
mhodo <mhodo1@etf.unsa.ba>
2018-05-07 23:06:41 +0200

    VS ignoring files <3

---
+++ Ambasada/Ambasada/Model/Prijava.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ambasada.Model
+{
+    class Prijava
+    {
+        // potrebno implementirati zajedno sa website-om naknadno, ne spada u projektni zadatak 6
+    }
+}
