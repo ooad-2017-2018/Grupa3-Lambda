441f6cec2ea31d9d1dc2e191f7e3503c2fdd2d91
ddzigal1 <ddzigal1@etf.unsa.ba>
2018-05-31 17:05:45 +0200

    Implementirane klasa Podnosilac, Prijava. Izbrisane klase Tombola i Uposlenik

--- AmbasadadotNET/AmbasadadotNET/Models/Podnosilac.cs
+++ AmbasadadotNET/AmbasadadotNET/Models/Podnosilac.cs
@@ -8,10 +8,16 @@ namespace AmbasadadotNET.Models
 {
     public class Podnosilac : Osoba
     {
-
+        string dodatneInformacije;
+        string mjestoPrebivalista;
         // potrebno implementirati zajedno sa website-om naknadno, ne spada u projektni zadatak 6
-        protected Podnosilac(int idOsobe, string naziv, string email, DateTime datumRodjenja, string jmbg) : base( naziv, email, datumRodjenja, jmbg)
+        protected Podnosilac(int idOsobe, string naziv, string email, DateTime datumRodjenja, string jmbg,string dodatneInfo, string mjestoPreb) : base( naziv, email, datumRodjenja, jmbg)
         {
+            dodatneInformacije = dodatneInfo;
+            mjestoPrebivalista = mjestoPreb;
         }
+
+        public string DodatneInformacije { get => dodatneInformacije; set => dodatneInformacije = value; }
+        public string MjestoPrebivalista { get => mjestoPrebivalista; set => mjestoPrebivalista = value; }
     }
 }
