1ce0aef68a880b4406d414688b1e4ce0cd5dc7c5
ddzigal1 <ddzigal1@etf.unsa.ba>
2018-06-03 15:26:58 +0200

    dizajn formi za uposlenike završen

--- Ambasada/Ambasada/VIew/UposleniciEdit.xaml.cs
+++ Ambasada/Ambasada/VIew/UposleniciEdit.xaml.cs
@@ -53,5 +53,6 @@ namespace Ambasada
         {
             this.Frame.Navigate(typeof(AdminPanel),viewmodel);
         }
+
     }
 }
--- Ambasada/Ambasada/VIew/UposlenikPage.xaml.cs
+++ Ambasada/Ambasada/VIew/UposlenikPage.xaml.cs
@@ -12,20 +12,51 @@ using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
-using Ambasada.ViewModel;
+using Ambasada.VIew;
 
 // The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=234238
 
-namespace Ambasada
+namespace Ambasada.VIew
 {
     /// <summary>
     /// An empty page that can be used on its own or navigated to within a Frame.
     /// </summary>
-    public partial class UposlenikPage : Page
+    public sealed partial class UposlenikPage : Page
     {
         public UposlenikPage()
         {
-          
+            this.InitializeComponent();
+        }
+
+        private void PrintVizeButton_Click(object sender, RoutedEventArgs e)
+        {
+            //uradi nešto što će se povezati sa printerom i isprintati
+        }
+
+        private void PotvrdiPrijavuButton_Click(object sender, RoutedEventArgs e)
+        {
+            //odobri selektovanu prijavu za tombolu
+        }
+
+        private void OdbijPrijavuButton_Click(object sender, RoutedEventArgs e)
+        {
+            //odbij selektovanu prijavu za tombolu
+        }
+
+        private void LogoutButton_Click(object sender, RoutedEventArgs e)
+        {
+            //logout korisnika, vraća na login page
+        }
+
+        private void PosaljiEmailButton_Click(object sender, RoutedEventArgs e)
+        {
+            //šalje sve na formu za popunjavanje emaila
+            this.Frame.Navigate(typeof(SlanjeEmail)); //fali helper kao drugi arg
+        }
+
+        private void DownloadPDFPrijaveButton_Click(object sender, RoutedEventArgs e)
+        {
+            //servis da se učita PDF fajl trenutačne prijave
         }
     }
 }
