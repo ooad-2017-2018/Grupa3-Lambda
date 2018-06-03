62758744459e7a1bcbc59c8d89e47f13e8f35a5b
Ali Dlakic <ali@pro.ba>
2018-06-03 19:14:38 +0200

    dodan cors. dot net je cool

--- AmbasadaAPI.NET/AmbasadaAPI.NET/App_Start/WebApiConfig.cs
+++ AmbasadaAPI.NET/AmbasadaAPI.NET/App_Start/WebApiConfig.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web.Http;
+using System.Web.Http.Cors;
 
 namespace AmbasadaAPI.NET
 {
@@ -19,6 +20,11 @@ namespace AmbasadaAPI.NET
                 routeTemplate: "api/{controller}/{id}",
                 defaults: new { id = RouteParameter.Optional }
             );
+
+        
+            var cors = new EnableCorsAttribute("*", "*", "*");
+            config.EnableCors(cors);
+
         }
     }
 }
