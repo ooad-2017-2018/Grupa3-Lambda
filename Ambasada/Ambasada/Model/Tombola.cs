6e3163d61e2ae6b9b26928c12ad16631066c4c0b
ddzigal1 <ddzigal1@etf.unsa.ba>
2018-06-03 16:14:28 +0200

    Svašta nešta

--- Ambasada/Ambasada/Model/Tombola.cs
+++ Ambasada/Ambasada/Model/Tombola.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,6 +9,7 @@ namespace Ambasada.Model
 {
     public class Tombola
     {
-        // potrebno implementirati zajedno sa website-om naknadno, ne spada u projektni zadatak 6
+        ObservableCollection<Prijava> lista = new ObservableCollection<Prijava>();
+
     }
 }
--- Ambasada/Ambasada/VIew/UposlenikPage.xaml.cs
+++ Ambasada/Ambasada/VIew/UposlenikPage.xaml.cs
@@ -14,6 +14,7 @@ using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
 using Ambasada.VIew;
 using Ambasada.ViewModel;
+using Ambasada.Model;
 
 // The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=234238
 
@@ -28,11 +29,14 @@ namespace Ambasada.VIew
         public UposlenikPage()
         {
             this.InitializeComponent();
+            viewmodel.inicijaliziraj();
         }
         protected override void OnNavigatedTo(NavigationEventArgs e)
         {
             base.OnNavigatedTo(e);
             viewmodel = (UposlenikViewModel)e.Parameter;
+            var lista = viewmodel.dajListuPrijava();
+            ListaPrijavljenihLB.ItemsSource = lista;
         }
         private void PrintVizeButton_Click(object sender, RoutedEventArgs e)
         {
@@ -42,16 +46,23 @@ namespace Ambasada.VIew
         private void PotvrdiPrijavuButton_Click(object sender, RoutedEventArgs e)
         {
             //odobri selektovanu prijavu za tombolu//
+            if (!(kliknuti is null)) {
+                kliknuti.stanjePrijave = true;
+            }
         }
 
         private void OdbijPrijavuButton_Click(object sender, RoutedEventArgs e)
         {
             //odbij selektovanu prijavu za tombolu
+            if (!(kliknuti is null)) {
+                kliknuti.stanjePrijave = false;
+            }
         }
 
         private void LogoutButton_Click(object sender, RoutedEventArgs e)
         {
             //logout korisnika, vraća na login page
+            this.Frame.Navigate(typeof(MainPage));
         }
 
         private void PosaljiEmailButton_Click(object sender, RoutedEventArgs e)
@@ -63,6 +74,16 @@ namespace Ambasada.VIew
         private void DownloadPDFPrijaveButton_Click(object sender, RoutedEventArgs e)
         {
             //servis da se učita PDF fajl trenutačne prijave
+        }
+        private Prijava kliknuti= new Prijava();
+        private void ListaPrijavljenihLB_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            kliknuti = (Prijava)ListaPrijavljenihLB.SelectedItem;
+            if (!(kliknuti is null)) {
+                ImeIPrezimeTB.Text = kliknuti.Podnosilac.naziv;
+                JMBGTB.Text = kliknuti.Podnosilac.jmbg;
+                EmailTB.Text = kliknuti.Podnosilac.email;
+            }
         }
     }
 }
