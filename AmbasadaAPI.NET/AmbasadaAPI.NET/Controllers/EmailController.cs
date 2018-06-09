c94cdfb7962fade6e1ff43da39453e78aacc7099
Ali Dlakic <ali@pro.ba>
2018-06-09 15:39:01 +0200

    Slanje emailova

---
+++ AmbasadaAPI.NET/AmbasadaAPI.NET/Controllers/EmailController.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Net.Mail;
+using System.Web.Http;
+using AmbasadaAPI.NET.Models;
+
+
+namespace AmbasadaAPI.NET.Controllers
+{
+    public class EmailController : ApiController
+    {
+        public HttpResponseMessage PostEmail(Email email)
+        {
+            try
+            {
+                SmtpClient smtpClient = new SmtpClient("elektrotehna.tk", 587)
+                {
+                    UseDefaultCredentials = false,
+                    Credentials = new System.Net.NetworkCredential("ambasada.elektrotehna", "[PASSWORD]"),
+                    DeliveryMethod = SmtpDeliveryMethod.Network,
+                    EnableSsl = true
+                };
+
+                MailMessage mail = new MailMessage
+                {
+                    From = new MailAddress("ambasada@elektrotehna.tk", "Ambasada Elektrotehne"),
+                    Subject = email.Subject,
+                    Body = email.Body
+                };
+                mail.To.Add(new MailAddress(email.Address));
+
+                smtpClient.Send(mail);
+            }
+            catch (Exception e)
+            {
+                Console.Out.Write(e);
+                return Request.CreateResponse(HttpStatusCode.InternalServerError, e.Message);
+            }
+            return Request.CreateResponse(HttpStatusCode.OK, "poslano");
+        }
+
+    }
+
+}
---
+++ AmbasadaAPI.NET/AmbasadaAPI.NET/Models/Email.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace AmbasadaAPI.NET.Models
+{
+    public class Email
+    {
+        public string Address { get; set; }
+        public string Subject { get; set; }
+        public string Body { get; set; }
+    }
+}
